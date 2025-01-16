20edb587162240831659a25a922fbec89eeb8072
Jack-Lacy <jlacy@uwo.ca>
2025-01-16 00:21:28 -0500

    changing zoom dynamically for mobile devices

--- src/componants/Display.tsx
+++ src/componants/Display.tsx
@@ -27,7 +27,7 @@ export default function Display(props: DisplayProps) {
                 <ambientLight intensity={0.5} />
                 <directionalLight position={[5, 5, 5]} />
                 {currentModel}
-                <OrbitControls minDistance={1} maxDistance={3} />
+                <OrbitControls minDistance={1} maxDistance={window.innerWidth > 500 ? 3 : 10} />
             </Canvas>
         </div>
     );
