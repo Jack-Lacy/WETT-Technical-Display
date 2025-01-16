769b7472d79f3903c9e352f64a1a009a1320063e
Jack-Lacy <jlacy@uwo.ca>
2025-01-16 18:49:33 -0500

    re-rendering the entire canvas on switch to eliminate bug where no model gets rendered

--- src/componants/Display.tsx
+++ src/componants/Display.tsx
@@ -23,7 +23,7 @@ export default function Display(props: DisplayProps) {
 
     return (
         <div className="display">
-            <Canvas className="canvas" style={{ height: '90vh' }}>
+            <Canvas className="canvas" style={{ height: '90vh' }} key={props.model}>
                 <ambientLight intensity={0.5} />
                 <directionalLight position={[5, 5, 5]} />
                 {currentModel}
