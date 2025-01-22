580852009114d70f6905fa7e3d79e96f4e87e5b1
Jack-Lacy <jlacy@uwo.ca>
2025-01-21 20:48:23 -0500

    minor changes

--- src/pages/Concrete.tsx
+++ src/pages/Concrete.tsx
@@ -56,7 +56,6 @@ export default function Concrete() {
                     </>
                     : null
                 }
-
             </div>
             <Display
                 model={model}
